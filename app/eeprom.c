42c52f4c705052985db7f6657afab40ca498b29d
Karel Blavka <karel.blavka@bigclown.com>
2017-10-29 16:49:11 +0100

    Add eeprom .c .h

---
+++ app/eeprom.c
@@ -0,0 +1,176 @@
+#include <eeprom.h>
+#include <usb_talk.h>
+
+static struct
+{
+    uint8_t alias_length;
+
+    uint8_t buffer[EEPROM_ALIAS_ROW_LENGTH + 1];
+
+} _eeprom;
+
+static int _eeprom_alias_find_position_for_id(uint64_t *id);
+static bool _eeprom_alias_length_set(uint8_t length);
+
+void eeprom_init(void)
+{
+    memset(&_eeprom, 0, sizeof(_eeprom));
+
+    uint8_t buffer[2];
+
+    bc_eeprom_read(EEPROM_ALIAS_ADDRESS_LENGTH, buffer, sizeof(buffer));
+
+    uint8_t neg = ~buffer[1];
+
+    if (buffer[0] == neg)
+    {
+        _eeprom.alias_length = buffer[0];
+    }
+}
+
+void eeprom_alias_add(uint64_t *id, char *name)
+{
+    int i = _eeprom_alias_find_position_for_id(id);
+
+    if (i == -1)
+    {
+        i = _eeprom.alias_length;
+    }
+
+    uint32_t address = EEPROM_ALIAS_ADDRESS_START + (i * EEPROM_ALIAS_ROW_LENGTH);
+
+    *((uint64_t *) &_eeprom.buffer) = *id;
+
+    memcpy(_eeprom.buffer + 8, name, 32);
+
+    if (!bc_eeprom_write(address, _eeprom.buffer, EEPROM_ALIAS_ROW_LENGTH))
+    {
+        usb_talk_send_format("[\"$eeprom/alias/add/error\", \"" USB_TALK_DEVICE_ADDRESS "\"]\n", *id);
+
+        return;
+    }
+
+    if (i == _eeprom.alias_length)
+    {
+        if (!_eeprom_alias_length_set(_eeprom.alias_length + 1))
+        {
+            usb_talk_send_format("[\"$eeprom/alias/add/error\", \"" USB_TALK_DEVICE_ADDRESS "\"]\n", *id);
+
+            return;
+        }
+    }
+
+    usb_talk_send_format("[\"$eeprom/alias/add/ok\", \"" USB_TALK_DEVICE_ADDRESS "\"]\n", *id);
+}
+
+void eeprom_alias_remove(uint64_t *id)
+{
+    int i = _eeprom_alias_find_position_for_id(id);
+
+    if (i != -1)
+    {
+        if ((i != _eeprom.alias_length) && (_eeprom.alias_length > 1))
+        {
+            // laste record
+            uint32_t address = EEPROM_ALIAS_ADDRESS_START + ((_eeprom.alias_length - 1) * EEPROM_ALIAS_ROW_LENGTH);
+
+            bc_eeprom_read(address, _eeprom.buffer, EEPROM_ALIAS_ROW_LENGTH);
+
+            address = EEPROM_ALIAS_ADDRESS_START + (i * EEPROM_ALIAS_ROW_LENGTH);
+
+            if (!bc_eeprom_write(address, _eeprom.buffer, EEPROM_ALIAS_ROW_LENGTH))
+            {
+                usb_talk_send_format("[\"$eeprom/alias/remove/error\", \"" USB_TALK_DEVICE_ADDRESS "\"]\n", *id);
+
+                return;
+            }
+        }
+
+        if (!_eeprom_alias_length_set(_eeprom.alias_length - 1))
+        {
+            usb_talk_send_format("[\"$eeprom/alias/remove/error\", \"" USB_TALK_DEVICE_ADDRESS "\"]\n", *id);
+
+            return;
+        }
+    }
+
+    usb_talk_send_format("[\"$eeprom/alias/remove/ok\", \"" USB_TALK_DEVICE_ADDRESS "\"]\n", *id);
+}
+
+void eeprom_alias_list(int page)
+{
+    if (page >= EEPROM_ALIAS_PAGE_LENGTH)
+    {
+        return;
+    }
+
+    int max_i = (page + 1) * EEPROM_ALIAS_ON_PAGE;
+
+    if (max_i >  _eeprom.alias_length)
+    {
+        max_i = _eeprom.alias_length;
+    }
+
+    uint32_t address;
+    bool comma = false;
+
+    usb_talk_message_start("$eeprom/alias/list/%d", page);
+    usb_talk_message_append("{");
+
+    for (int i = page * EEPROM_ALIAS_ON_PAGE; i < max_i; i++)
+    {
+        address = EEPROM_ALIAS_ADDRESS_START + (i * EEPROM_ALIAS_ROW_LENGTH);
+
+        bc_eeprom_read(address, _eeprom.buffer, EEPROM_ALIAS_ROW_LENGTH);
+
+        if (comma)
+        {
+            usb_talk_message_append(",");
+        }
+
+        usb_talk_message_append("\"" USB_TALK_DEVICE_ADDRESS "\": \"%s\"", *((uint64_t *)_eeprom.buffer), _eeprom.buffer + 8);
+
+        comma = true;
+    }
+
+    usb_talk_message_append("}");
+
+    usb_talk_message_send();
+}
+
+static int _eeprom_alias_find_position_for_id(uint64_t *id)
+{
+    uint32_t address = EEPROM_ALIAS_ADDRESS_START;
+    uint64_t tmp_id;
+
+    for (int i = 0; i < _eeprom.alias_length; i++)
+    {
+        bc_eeprom_read(address, &tmp_id, sizeof(tmp_id));
+
+        if (tmp_id == *id)
+        {
+            return i;
+        }
+
+        address += EEPROM_ALIAS_ROW_LENGTH;
+    }
+
+    return -1;
+}
+
+static bool _eeprom_alias_length_set(uint8_t length)
+{
+    uint8_t buffer[2];
+
+    buffer[0] = length;
+    buffer[1] = ~length;
+
+    if (!bc_eeprom_write(EEPROM_ALIAS_ADDRESS_LENGTH, buffer, sizeof(buffer)))
+    {
+        return false;
+    }
+
+    _eeprom.alias_length = length;
+
+    return true;
+}
---
+++ app/eeprom.h
@@ -0,0 +1,23 @@
+#ifndef APP_EEPROM_H
+#define APP_EEPROM_H
+
+#include <bc_eeprom.h>
+
+#define EEPROM_ALIAS_ADDRESS_LENGTH    0x00
+#define EEPROM_ALIAS_ADDRESS_START     0x04
+#define EEPROM_ALIAS_NAME_LENGTH       32
+#define EEPROM_ALIAS_ROW_LENGTH        (8 + EEPROM_ALIAS_NAME_LENGTH)
+#define EEPROM_ALIAS_ON_PAGE           8
+#define EEPROM_ALIAS_PAGE_LENGTH       4
+
+
+void eeprom_init(void);
+
+void eeprom_alias_add(uint64_t *id, char *name);
+
+void eeprom_alias_remove(uint64_t *id);
+
+void eeprom_alias_list(int page);
+
+
+#endif // APP_EEPROM_H
